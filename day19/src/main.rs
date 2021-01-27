9e035c234fe16fb3a787d1912e910b09608d12f1
Mridul Pentapalli <mridulp@gmail.com>
2021-01-27 23:31:30 +0530

    Solved day 19 - 2 finally

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -33,11 +33,19 @@ struct MatchRule {
 
 impl MatchRule {
     fn consume(&self, input: &str, offset: usize) -> (bool, usize) {
+
+        // match self.no {
+        //     8|11|42|31 => println!("input: {} of len: {}. Offset: {}. rule: {}", input, input.len(), offset, self.no),
+        //     _ => {},
+        // };
+
         match &self.rule_type {
-            RuleType::Prim(c) => if input.chars().nth(offset).unwrap() == *c {
-                return (true, offset + 1);
-            } else {
-                return (false, offset);
+            RuleType::Prim(c) => {
+                if input.chars().nth(offset).unwrap() == *c {
+                    return (true, offset + 1);
+                } else {
+                    return (false, offset);
+                }
             },
             RuleType::Or{left, right} => {
                 let (success, next_offset) = left.consume(input, offset);
@@ -94,7 +102,7 @@ fn parse_rules() -> HashMap<i32, RuleTypeParsed> {
         let rule_type = inners_iter.next().unwrap();
         match rule_type.as_rule() {
             Rule::and => {
-                println!("{}", rule_number);
+                // println!("{}", rule_number);
                 let subrules: Vec<i32> = rule_type.as_str().split(" ").map(|s| s.parse::<i32>().unwrap()).collect();
                 rules.insert(rule_number, RuleTypeParsed::And(subrules));
             },
@@ -129,6 +137,7 @@ fn make_rule_zero(parsed_rules: &HashMap<i32, RuleTypeParsed>) -> MatchRule {
 
 fn make_rule(rule_number: i32, parsed_rules: &HashMap<i32, RuleTypeParsed>) -> MatchRule {
     let parsed_rule = parsed_rules.get(&rule_number).unwrap();
+
     match parsed_rule {
         RuleTypeParsed::Prim(c) => {
             return MatchRule{no: rule_number, rule_type: RuleType::Prim(*c)};
@@ -147,29 +156,86 @@ fn make_rule(rule_number: i32, parsed_rules: &HashMap<i32, RuleTypeParsed>) -> M
     }
 }
 
+fn is_valid_part_2(input: &str, rule_42: &MatchRule, rule_31: &MatchRule) -> bool {
+    let mut next_offset = 0;
+    let mut count_42 = 0;
+    let mut count_31 = 0;
+
+    //For Part 2, thanks to the same input, and some reddit 
+    //explanations, the easiest way to solve this is that
+    //rule 0: 8 11, and 8: 42 | 42 8 and 11: 42 31 | 42 11 31.
+    //So instead of matching against rule 0, we repeatedly 
+    //match against rule 42 and then rule 31. If the count of 42 matches
+    // >= count of rule 31 matches, then the string is valid.
+
+    loop {
+        let (success, offset) = rule_42.consume(input, next_offset);
+        next_offset = offset;
+        if next_offset >= input.len() {
+            return false;
+        }
+
+        if !success {
+            break;
+        } else {
+            count_42 += 1;
+        }
+    }
+
+    if next_offset == input.len() {
+        return false;
+    }
+
+    loop {
+        let (success, offset) = rule_31.consume(input, next_offset);
+        if !success {
+            return false
+        } else {
+            next_offset = offset;
+            count_31 += 1;
+            if offset == input.len() {
+                return count_42 > count_31;
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), std::io::Error> {
     let data_file = File::open("data.txt")?;
 
     let parsed_rules = parse_rules();
     let zero_rule = make_rule_zero(&parsed_rules);
 
-    println!("{:?}", zero_rule);
+    // println!("{:?}", zero_rule);
 
-    let mut valid_count = 0;
+    let rule_42 = make_rule(42, &parsed_rules); 
+    let rule_31 = make_rule(31, &parsed_rules);
+
+    let mut valid_count_1 = 0;
+    let mut valid_count_2 = 0;
 
     let data = io::BufReader::new(data_file);
     for line in data.lines() {
         let line = line.unwrap();
+        // println!("Line: {}", line);
         let (valid, end_offset) = zero_rule.consume(line.as_str(), 0);
         if valid && end_offset == line.len() {
-            println!("Valid: {}", line);
-            valid_count += 1;
+            // println!("Valid: {}", line);
+            valid_count_1 += 1;
         } else {
-            println!("Invalid: {}", line);
+            // println!("Invalid: {}", line);
+        }
+
+        if is_valid_part_2(line.as_str(), &rule_42, &rule_31) {
+            println!("Valid: {}", line.as_str());
+            valid_count_2 += 1;
+        } else {
+            println!("Invalid: {}", line.as_str());
         }
     }
 
-    println!("Part1: {}", valid_count);
+    println!("Part1: {}", valid_count_1);
+    println!("Part2: {}", valid_count_2);
 
     Ok(())
 }
